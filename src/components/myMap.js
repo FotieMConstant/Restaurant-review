cadf7a00fb90ddc2be0c2662ae80465c312b9582
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-06-25 18:38:45 +0100

    Modified the map component and replaced leafletjs with Googlemapspai

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import styled from 'styled-components';
+import React from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
-const Wrapper = styled.div`
-  width: ${props => props.width}px;
-  height: ${props => props.height}px;
-  z-index: 1;
-`;
+function myMap(){
+const MyMapComponent = compose(
+  withProps({
+    /**
+     * Fetching data from googleapis with API key
+     */
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `150%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `150%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 3.846963, lng: 11.55072 }}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: 3.846963, lng: 11.55072 }} />
+    )}
+  </GoogleMap>
+));
 
-export default class Map extends React.Component {
+return (
+ <div> 
+   <MyMapComponent isMarkerShown />
+ </div>);
+    }
 
-  componentDidMount(){
-    this.map = L.map('map', {
-      center: [3.846963, 11.55072],
-      zoom: 12,
-     
-    });
-    L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=e8LZc57PV2AwP7LJPKLT', {
-      attribution: 'Webapp by Fotie <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-  }).addTo(this.map);
-
-
-  var myIcon = L.icon({
-    iconUrl: 'http://localhost:3000/assets/images/icons/me.png',
-    iconSize: [35, 35],
-    iconAnchor: [21, 81],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-});
-L.marker([3.846963, 11.55072], {icon: myIcon}).addTo(this.map)
-    .bindPopup('Your location.')
-    .openPopup();
-
-  }
-
-
-  render(){
-    return <Wrapper width ="900" height="596" id="map"/>
-  }
-}
+export default myMap
