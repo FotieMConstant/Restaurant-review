f928f3b483ca3e5a46fa28f54e4631b6aab9cfe4
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-07-04 22:47:07 +0100

    Map eventListener
    Clicking on any position of the map returns the long and lat of the clicked location... this is for adding a restaurant

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,60 +1,64 @@
-import React from 'react';
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { useEffect } from 'react';
+import React from 'react'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
+import { useEffect } from 'react'
 // Improting ratings component
-import Rating from '@material-ui/lab/Rating';
+import Rating from '@material-ui/lab/Rating'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      maxWidth: '36ch',
-      backgroundColor: theme.palette.background.paper,
-    },
-    inline: {
-      display: 'inline',
-    },
-    large: {
-      width: theme.spacing(11),
-      height: theme.spacing(11),
-      'margin-top': '8px'
-    },
-    profile:{
-        'margin-right': '-120px !important'
-    },
-  }));
-
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+  large: {
+    width: theme.spacing(11),
+    height: theme.spacing(11),
+    'margin-top': '8px',
+  },
+  profile: {
+    'margin-right': '-120px !important',
+  },
+  wrapper: {
+    'overflow-y': 'scroll',
+    height: '525px',
+  },
+}))
 
 function ContentFeed() {
-    const classes = useStyles();
-   const [Feeds, setFeeds] = React.useState([]);
-  
-    useEffect(() =>{
-      // axios.get(`http://localhost:3000/api/restaurants.json`)
-        axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=4.051056,9.767869&radius=1500&type=restaurant&key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU`)
-        .then(res => {
-          let Feeds = res.data.results;
-          console.log(Feeds);
-          setFeeds(Feeds);
-        })
-    }, [])
+  const classes = useStyles()
+  const [Feeds, setFeeds] = React.useState([])
 
-        return (
-          <div>
-           <Divider/>
-            { Feeds.map((Feed, index) => 
-            <div key={index}>
+  useEffect(() => {
+    // axios.get(`http://localhost:3000/api/restaurants.json`)
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=4.051056,9.767869&radius=1500&type=restaurant&key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU`
+      )
+      .then(res => {
+        let Feeds = res.data.results
+        console.log(Feeds)
+        setFeeds(Feeds)
+      })
+  }, [])
 
-            <List className={classes.root}>
+  return (
+    <div className={classes.wrapper}>
+      <Divider />
+      {Feeds.map((Feed, index) => (
+        <div key={index}>
+          <List className={classes.root}>
             <ListItem alignItems="flex-start">
-         
               <ListItemText
                 secondary={
                   <React.Fragment>
@@ -64,37 +68,42 @@ function ContentFeed() {
                       className={classes.inline}
                       color="textPrimary"
                     >
-                       <span>
-                       <h3>{Feed.name.substring(0,35)}</h3>
-                         <i>{Feed.vicinity.substring(0,40)}...</i><br/>
-                       </span>
+                      <span>
+                        <h3>{Feed.name.substring(0, 35)}</h3>
+                        <i>{Feed.vicinity.substring(0, 40)}...</i>
+                        <br />
+                      </span>
                     </Typography>
                     <div className="wrapper">
                       <div className={classes.root}>
-                      <span>{Feed.rating}</span> <Rating name="size-small" defaultValue={Feed.rating} size="small" readOnly/>
-
+                        <span>{Feed.rating}</span>{' '}
+                        <Rating
+                          name="size-small"
+                          defaultValue={Feed.rating}
+                          size="small"
+                          readOnly
+                        />
                       </div>
                     </div>
-
                   </React.Fragment>
                 }
               />
-              <div  className={classes.profile}>
-              <ListItemAvatar>
-                <Avatar className={classes.large} alt={Feed.name} src={Feed.icon} />
-              </ListItemAvatar>
+              <div className={classes.profile}>
+                <ListItemAvatar>
+                  <Avatar
+                    className={classes.large}
+                    alt={Feed.name}
+                    src={Feed.icon}
+                  />
+                </ListItemAvatar>
               </div>
             </ListItem>
-            <Divider/>
-
+            <Divider />
           </List>
-          </div>
-            )}
-          </div>
-        )
-      
-   
+        </div>
+      ))}
+    </div>
+  )
 }
-
 
 export default ContentFeed
--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,64 +1,79 @@
-import React from "react";
-import { compose, withProps } from "recompose";
+import React from 'react'
+import { compose, withProps } from 'recompose'
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
-} from "react-google-maps";
+  Marker,
+} from 'react-google-maps'
+import FormDialog from './AddRestaurant'
 
-function myMap(){
-
+function myMap() {
+  function handleClick(event) {
+    var lat = event.latLng.lat(),
+      lng = event.latLng.lng()
+    console.log('You clicked on the coordinates => log: ' + lng + ' lat:' + lat)
+    return (
+      <div>
+        {<FormDialog />}
+      </div>
+    )
+  }
   //My global variables for long and lat
-  let longitude;
-  let latitude;
+  let longitude
+  let latitude
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
-    maximumAge: 0
-  };
-  
+    maximumAge: 0,
+  }
+
   function success(pos) {
-    var crd = pos.coords;
-    longitude = crd.longitude;
-    latitude = crd.latitude;
+    var crd = pos.coords
+    longitude = crd.longitude
+    latitude = crd.latitude
 
-    console.log('Your current position is:');
-    console.log(`Latitude : ${latitude}`);
-    console.log(`Longitude: ${longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+    console.log('Your current position is:')
+    console.log(`Latitude : ${latitude}`)
+    console.log(`Longitude: ${longitude}`)
+    console.log(`More or less ${crd.accuracy} meters.`)
   }
-  
+
   function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.warn(`ERROR(${err.code}): ${err.message}`)
   }
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
-const MyMapComponent = compose(
-  withProps({
-    /**
-     * Fetching data from googleapis with API key
-     */
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `150%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `150%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={10} defaultCenter={{ lat: latitude, lng: longitude }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: latitude, lng: longitude }} />
-    )}
-  </GoogleMap>
-));
 
-return (
- <div> 
-   <MyMapComponent isMarkerShown />
- </div>);
-    } 
+  navigator.geolocation.getCurrentPosition(success, error, options)
+  const MyMapComponent = compose(
+    withProps({
+      /**
+       * Fetching data from googleapis with API key
+       */
+      googleMapURL:
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU&v=3.exp&libraries=geometry,drawing,places',
+      loadingElement: <div style={{ height: `150%` }} />,
+      containerElement: <div style={{ height: `400px` }} />,
+      mapElement: <div style={{ height: `150%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+  )(props => (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: latitude, lng: longitude }}
+      onClick={e => handleClick(e)}
+    >
+      {props.isMarkerShown && (
+        <Marker position={{ lat: latitude, lng: longitude }} />
+      )}
+    </GoogleMap>
+  ))
+
+  return (
+    <div>
+      <MyMapComponent isMarkerShown />
+    </div>
+  )
+}
 
 export default myMap
