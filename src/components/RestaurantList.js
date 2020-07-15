d3831a7ec92f64196a81cb72de6bba9d2bd202a0
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-07-15 19:21:59 +0100

    Working on fetching places details
    I can now fetch and display basic user info, in the review dropdown section, however still working to making it dynamic per each restaurant

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,136 +1,243 @@
-import React from 'react'
-import axios from 'axios'
-import { makeStyles } from '@material-ui/core/styles'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import Divider from '@material-ui/core/Divider'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import Avatar from '@material-ui/core/Avatar'
-import Typography from '@material-ui/core/Typography'
-import { useEffect, useState } from 'react'
+import React from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { useEffect, useState } from "react";
 // Improting ratings component
-import Rating from '@material-ui/lab/Rating'
+import Rating from "@material-ui/lab/Rating";
+import Card from "@material-ui/core/Card";
+import clsx from "clsx";
+import CardContent from "@material-ui/core/CardContent";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import CardActions from "@material-ui/core/CardActions";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    maxWidth: '36ch',
+    width: "100%",
+    maxWidth: "36ch",
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
-    display: 'inline',
+    display: "inline",
   },
   large: {
     width: theme.spacing(11),
     height: theme.spacing(11),
-    'margin-top': '8px',
+    "margin-top": "8px",
   },
   profile: {
-    'margin-right': '-120px !important',
+    "margin-right": "-120px !important",
   },
   wrapper: {
-    'overflow-y': 'scroll',
-    height: '525px',
+    "overflow-y": "scroll",
+    height: "525px",
   },
-}))
+  reviews: {
+    maxWidth: "10%",
+  },
+}));
 
 function ContentFeed() {
-  const classes = useStyles()
-  const [Feeds, setFeeds] = useState([])
+  const classes = useStyles();
+  const [Feeds, setFeeds] = useState([]);
 
-   //State for the set lng and lat when getting location
-   const [longitude, setLongitude] = useState(0);
-   const [latitude, setLatitude] = useState(0);
+  // State for place_id
+  const [placeId, setPlaceId] = useState(null);
 
-   useEffect(()=>{
+  //State for details
+  const [Details, setDetail] = useState(null);
+
+  //State for the set lng and lat when getting location
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = (place_id) => {
+    setExpanded(!expanded);
+    setPlaceId(place_id);
+  };
+
+  //Fetching for places details
+  useEffect(() => {
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJf4puqOrbixAReA-2Dr_AwWE&fields=name,rating,reviews,formatted_phone_number&key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU`
+      )
+      .then((res) => {
+        let Detail = res.data.result;
+        console.log(JSON.stringify(res.data.result));
+        setDetail(Detail);
+        console.log("Your Resto ID is => " + placeId);
+        console.log("HERE ARE THE DETAILS " + Detail);
+      });
+  }, []);
+
+  //Getting the user's position
+  useEffect(() => {
     if ("geolocation" in navigator) {
       console.log("Available");
       navigator.geolocation.getCurrentPosition(
-          function(position) {
-              setLongitude(position.coords.longitude);
-              setLatitude(position.coords.latitude);
-          },
-          function(error) {
-              console.error("Error Code = " + error.code + " - " + error.message);
-          }
-      )
-      navigator.geolocation.watchPosition(function(position) {
-          console.log("WatchPosition => Latitude is :", position.coords.latitude);
-          console.log("WatchPosition => Longitude is :", position.coords.longitude);
+        function (position) {
+          setLongitude(position.coords.longitude);
+          setLatitude(position.coords.latitude);
+        },
+        function (error) {
+          console.error("Error Code = " + error.code + " - " + error.message);
+        }
+      );
+      navigator.geolocation.watchPosition(function (position) {
+        console.log("WatchPosition => Latitude is :", position.coords.latitude);
+        console.log(
+          "WatchPosition => Longitude is :",
+          position.coords.longitude
+        );
       });
-  }else{
+    } else {
       console.log("Not Available");
-  }
-},[]);
+    }
+  }, []);
 
-
-
+  //Fetching NearbyPlaces
   useEffect(() => {
     // axios.get(`http://localhost:3000/api/restaurants.json`)
     axios
       .get(
         `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=2000&type=restaurant&key=AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU`
       )
-      .then(res => {
-        let Feeds = res.data.results
-        console.log(Feeds)
-        setFeeds(Feeds)
-      })
-  }, [latitude, longitude])
+      .then((res) => {
+        let Feeds = res.data.results;
+        console.log(Feeds);
+        setFeeds(Feeds);
+      });
+  }, [latitude, longitude]);
 
   return (
-    <div className={classes.wrapper}>
-      <Divider />
-      {Feeds.map((Feed, index) => (
-        <div key={index}>
-          <List className={classes.root}>
-            <ListItem alignItems="flex-start">
-              <ListItemText
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                      <span>
-                        <h3>{Feed.name.substring(0, 35)}</h3>
-                        <i>{Feed.vicinity.substring(0, 40)}...</i>
-                        <br />
-                      </span>
-                    </Typography>
-                    <div className="wrapper">
-                      <div className={classes.root}>
-                        <span>{Feed.rating}</span>{' '}
-                        <Rating
-                          name="size-small"
-                          defaultValue={Feed.rating}
-                          size="small"
-                          readOnly
-                        />
+    <div>
+      <Card className={classes.wrapper}>
+        {Feeds.map((Feed, index) => (
+          <div key={index}>
+            <List className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemText
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary"
+                      >
+                        <span>
+                          <h3>{Feed.name.substring(0, 35)}</h3>
+                          <i>{Feed.vicinity.substring(0, 40)}...</i>
+                          <br />
+                        </span>
+                      </Typography>
+                      <div className="wrapper">
+                        <div className={classes.root}>
+                          <span>{Feed.rating}</span>
+                          <Rating
+                            name="half-rating"
+                            precision={0.5}
+                            defaultValue={Feed.rating}
+                            size="small"
+                            readOnly
+                          />
+                        </div>
                       </div>
-                    </div>
-                  </React.Fragment>
-                }
-              />
-              <div className={classes.profile}>
-                <ListItemAvatar>
-                  <Avatar
-                    className={classes.large}
-                    alt={Feed.name}
-                    src={Feed.icon}
-                  />
-                </ListItemAvatar>
-              </div>
-            </ListItem>
-            <Divider />
-          </List>
-        </div>
-      ))}
+                    </React.Fragment>
+                  }
+                />
+                <div className={classes.profile}>
+                  <ListItemAvatar>
+                    <Avatar
+                      className={classes.large}
+                      alt={Feed.name}
+                      src={Feed.icon}
+                    />
+                  </ListItemAvatar>
+                </div>
+              </ListItem>
+              <CardActions disableSpacing>
+                <IconButton
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                    index,
+                  })}
+                  onClick={() => handleExpandClick(Feed.place_id)}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon size="small" className={classes.dropper} />
+                </IconButton>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Grid container item xs={3} spacing={3}>
+                    {/* Displaying reviews */}
+                    <List className={classes.reviews}>
+                      {/* Start of Reviews */}
+                      {Details.reviews.map((Detail, index) => (
+                        <ListItem alignItems="flex-start">
+                          <ListItemAvatar>
+                            <Avatar
+                              alt="Remy Sharp"
+                              src={Detail.profile_photo_url}
+                            />
+                          </ListItemAvatar>
+                          <ListItemText
+                            primary={Detail.author_name}
+                            secondary={
+                              <React.Fragment>
+                                <Typography
+                                  component="span"
+                                  variant="body2"
+                                  className={classes.inline}
+                                  color="textPrimary"
+                                >
+                                  <Rating
+                                    name="half-rating"
+                                    precision={0.5}
+                                    defaultValue={Detail.rating}
+                                    size="small"
+                                    readOnly
+                                  />
+                                </Typography>
+                                <br />
+                                {Detail.text} <br />{" "}
+                                <em>{Detail.relative_time_description}</em>
+                              </React.Fragment>
+                            }
+                          />
+                        </ListItem>
+                      ))}
+
+                      {/* End of Reviews */}
+
+                      <Divider variant="inset" component="li" />
+                    </List>
+                    {/* End of review list */}
+                  </Grid>
+                </CardContent>
+              </Collapse>
+              <br />
+              <Divider />
+            </List>
+          </div>
+        ))}
+      </Card>
     </div>
-  )
+  );
 }
 
-export default React.memo(ContentFeed)
+export default React.memo(ContentFeed);
--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,21 +1,20 @@
-import React, {useEffect, useState} from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect, useState } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 //Styling for the map container
 const containerStyle = {
-  width: 'auto',
-  height: '620px'
+  width: "auto",
+  height: "620px",
 };
-
 
 function MyComponent() {
   //State for the set lng and lat when getting location
@@ -30,115 +29,122 @@ function MyComponent() {
   //Funtions to hangle Opena and Close of modal
   const handleOpen = () => {
     setOpen(true);
-   };
+  };
 
-   const handleClose = () => {
-       setOpen(false);
-   };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  const mapClick = (e) =>{
-    setnewRestaurant((previousState)=>[
+  const mapClick = (e) => {
+    console.log(e.latLng);
+    setnewRestaurant((previousState) => [
       ...previousState,
       {
         lat: e.latLng.lat(),
-        lng: e.latLng.lng()
-      }
-    ])
+        lng: e.latLng.lng(),
+      },
+    ]);
     handleOpen(); //Calling the modal when the user clicks
-    console.log("You clicked on the coordinates => lng: " + newRestaurant.lng + " lat:" + newRestaurant.lat);
-  }
+    console.log(
+      "You clicked on the coordinates => lng: " +
+        newRestaurant.lng +
+        " lat:" +
+        newRestaurant.lat
+    );
+  };
 
   //Getting the user's coordinates // Lng and Lat
-  useEffect(()=>{
+  useEffect(() => {
     if ("geolocation" in navigator) {
       console.log("Available");
       navigator.geolocation.getCurrentPosition(
-          function(position) {
-              console.log("Latitude is :", position.coords.latitude);
-              console.log("Longitude is :", position.coords.longitude);
-              setLongitude(position.coords.longitude);
-              setLatitude(position.coords.latitude);
-          },
-          function(error) {
-              console.error("Error Code = " + error.code + " - " + error.message);
-          }
-      )
-      navigator.geolocation.watchPosition(function(position) {
+        function (position) {
           console.log("Latitude is :", position.coords.latitude);
           console.log("Longitude is :", position.coords.longitude);
+          setLongitude(position.coords.longitude);
+          setLatitude(position.coords.latitude);
+        },
+        function (error) {
+          console.error("Error Code = " + error.code + " - " + error.message);
+        }
+      );
+      navigator.geolocation.watchPosition(function (position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
       });
-  }else{
+    } else {
       console.log("Not Available");
-  }
-},[]);
+    }
+  }, []);
 
-const center = {
-  lat: latitude,
-  lng: longitude
-};
-const userCurrentPosition = {
-  lat: latitude,
-  lng: longitude
-}
+  const center = {
+    lat: latitude,
+    lng: longitude,
+  };
+  const userCurrentPosition = {
+    lat: latitude,
+    lng: longitude,
+  };
   return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU"
-    >
+    <LoadScript googleMapsApiKey="AIzaSyD4p0gchCyP98IGwRwGes-UGx4BDEqDrjU">
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={15}
-        onClick={e => mapClick(e)}
+        onClick={(e) => mapClick(e)}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Add new restaurant</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Enter restaurant details below
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Restaurant name"
-            type="text"
-            fullWidth
-          />
+        {/* Child components, such as markers, info windows, etc. */}
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add new restaurant</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter restaurant details below
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Restaurant name"
+              type="text"
+              fullWidth
+            />
 
-          <TextField
-            margin="dense"
-            id="description"
-            label="Description"
-            type="text"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            ADD
-          </Button>
-        </DialogActions>
-      </Dialog>
+            <TextField
+              margin="dense"
+              id="description"
+              label="Description"
+              type="text"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              ADD
+            </Button>
+          </DialogActions>
+        </Dialog>
         {
-          newRestaurant.map((position)=>(
+          //Adding a marker to the clicked position
+          newRestaurant.map((position) => (
             <Marker
               key={`${position.lat}-${position.lng}`}
-              position={{ lat:position.lat, lng:position.lng }}
+              position={{ lat: position.lat, lng: position.lng }}
             />
-           ))
-         }
-
+          ))
+        }
 
         {/* Displaying a marker on the user's location with the position props */}
         <Marker position={userCurrentPosition} />
-  
       </GoogleMap>
     </LoadScript>
-  )
+  );
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);
